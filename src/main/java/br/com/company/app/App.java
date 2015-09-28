7f241aca19a107afe31db4622cf0803e3a2658ef
juliano <juliano@juliano-HP-Pavilion-dv2000-GA478UAR-ABA>
2015-09-28 09:05:56 -0300

    Second commit: class App added and method main() created

---
+++ src/main/java/br/com/company/app/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.company.app;
+
+/**
+ *
+ * @author juliano
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+    
+}
